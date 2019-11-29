d270dd2023b13cd83e94bcd97ddebd319493da8a
Lakshay Bhambri <lakshay.bhambri@go-jek.com>
2019-11-29 17:01:24 +0530

    Made service area and type apis

--- handlers/handler.go
+++ handlers/handler.go
@@ -7,6 +7,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
+const (
+	LOW_SUPPLY = "1"
+	RAIN_CHECK = "2"
+)
+
 type Metric struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
@@ -16,6 +21,16 @@ type MetricRecommendation struct {
 	ID       string `json:"id"`
 	MetricID string `json:"metric_id"`
 	Name     string `json:"name"`
+}
+
+type ServiceArea struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type ServiceType struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 func GetMetrics(w http.ResponseWriter, r *http.Request) {
@@ -56,6 +71,44 @@ func GetMetricRecommendations(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetMetricsData(w http.ResponseWriter, r *http.Request) {
-	// vars := mux.Vars(r)
+	vars := mux.Vars(r)
+	switch vars["id"] {
+	case LOW_SUPPLY:
+
+		break
+	case RAIN_CHECK:
+
+		break
+	}
 	executeQuery(w, "select * from shapes")
+}
+
+func GetServiceAreas(w http.ResponseWriter, r *http.Request) {
+	data, err := ReadFile("service_areas")
+	if err != nil {
+		Error(w, err)
+		return
+	}
+	serviceAreas := []ServiceArea{}
+	err = json.Unmarshal(data, &serviceAreas)
+	if err != nil {
+		Error(w, err)
+		return
+	}
+	Ok(w, serviceAreas)
+}
+
+func GetServiceTypes(w http.ResponseWriter, r *http.Request) {
+	data, err := ReadFile("service_types")
+	if err != nil {
+		Error(w, err)
+		return
+	}
+	serviceTypes := []ServiceType{}
+	err = json.Unmarshal(data, &serviceTypes)
+	if err != nil {
+		Error(w, err)
+		return
+	}
+	Ok(w, serviceTypes)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -8,4 +8,6 @@ func RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/metrics", handler.GetMetrics).Methods("GET")
 	router.HandleFunc("/metrics/{id}/recommendations", handler.GetMetricRecommendations).Methods("GET")
 	router.HandleFunc("/metrics/{id}/data", handler.GetMetricsData).Methods("GET")
+	router.HandleFunc("/service-types", handler.GetServiceTypes).Methods("GET")
+	router.HandleFunc("/service-areas", handler.GetServiceAreas).Methods("GET")
 }
