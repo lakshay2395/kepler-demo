3368cbbfd739f9806595f8c89e56d25ca3ce08bd
kshitij.singh <kshitij.singh@go-jek.com>
2019-11-29 15:08:10 +0530

    refactored code

--- handlers/handler.go
+++ handlers/handler.go
@@ -12,6 +12,23 @@ import (
 //Default db name
 var DB_NAME string = "stargate_ui"
 
+func CreateDB() {
+	db := db.GetClient()
+	response, err := db.Query(client.Query{
+		Command:  "create database stargate_ui",
+		Database: DB_NAME,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if response.Error() != nil {
+		fmt.Println(response.Error())
+	}
+	fmt.Println("Created dB succesfully")
+	return
+}
+
 func GetTripsList(w http.ResponseWriter, r *http.Request) {
 	executeQuery(w, "select * from shapes")
 }
@@ -40,15 +57,6 @@ func executeQuery(w http.ResponseWriter, query string) {
 		return
 	}
 	Ok(w, response.Results)
-}
-
-func Error(w http.ResponseWriter, err error) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusInternalServerError)
-	response := map[string]string{}
-	response["error"] = fmt.Sprintf("%s", err)
-	payload, _ := json.Marshal(response)
-	w.Write(payload)
 }
 
 func Ok(w http.ResponseWriter, response interface{}) {
--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
-	"github.com/lakshay2395/kepler-demo/repository"
+	"github.com/gorilla/mux"
+	"github.com/lakshay2395/kepler-demo/db"
+	"github.com/lakshay2395/kepler-demo/routes"
+	handler "github.com/lakshay2395/kepler-demo/handlers"
 )
 
 func initEnv() {
@@ -15,21 +20,15 @@ func initEnv() {
 	os.Setenv("PORT", "8080")
 }
 
-// func main() {
-// 	initEnv() //TODO: to remove after finalization
-// 	err := db.Init(os.Getenv("INFLUX_HOST"), os.Getenv("INFLUX_PORT"), os.Getenv("INFLUX_USERNAME"), os.Getenv("INFLUX_PASSWORD"))
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	r := mux.NewRouter()
-// 	routes.RegisterRoutes(r)
-// 	http.Handle("/", r)
-// 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), r))
-// }
-
 func main() {
-
-	conClient := repository.CreateClient()
-	repository.ExecuteQuery("create database stargate_ui", conClient)
-	fmt.Println()
+	initEnv() //TODO: to remove after finalization
+	handler.CreateDB()
+	err := db.Init(os.Getenv("INFLUX_HOST"), os.Getenv("INFLUX_PORT"), os.Getenv("INFLUX_USERNAME"), os.Getenv("INFLUX_PASSWORD"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := mux.NewRouter()
+	routes.RegisterRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), r))
 }
