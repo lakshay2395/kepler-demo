d2e568562f4b3ccdf764b003df4ca61f3e1377ef
kshitij.singh <kshitij.singh@go-jek.com>
2019-11-29 14:06:31 +0530

    Added db client to create table

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	repo "github.com/lakshay2395/kepler-demo/repository"
 )
 
 func main() {
+
+	conClient := repo.CreateClient()
+	repo.ExecuteQuery("create database stargate_ui", conClient)
 	fmt.Println()
 }
