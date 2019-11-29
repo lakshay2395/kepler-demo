334d7c3e9518d6182d1509ac072a2a43eb8bb579
kshitij.singh <kshitij.singh@go-jek.com>
2019-11-29 21:01:41 +0530

    added api for low supply and rain check

--- handlers/handler.go
+++ handlers/handler.go
@@ -77,16 +77,22 @@ func GetMetricsData(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	serviceArea := r.URL.Query()["serviceArea"]
 	serviceType := r.URL.Query()["serviceType"]
-	fmt.Println("select * from shapes where serviceArea = " + serviceArea[0] + " and serviceType = " + serviceType[0])
+	var query string
 	switch vars["id"] {
 	case LOW_SUPPLY:
-
+		query = "select * from low_supply where service_area='" + serviceArea[0] + "' and service_type='" + serviceType[0] + "'"
 		break
 	case RAIN_CHECK:
-
+		query = "select * from rain_check where service_area='" + serviceArea[0] + "' and service_type='" + serviceType[0] + "'"
 		break
 	}
-	executeQuery(w, "select * from shapes")
+	response := getResponse(query)[0]
+	columns := response.Series[0].Columns
+	values := response.Series[0].Values[0]
+
+	data := createMap(columns, values)
+
+	Ok(w, data)
 }
 
 func GetServiceAreas(w http.ResponseWriter, r *http.Request) {
@@ -117,4 +123,13 @@ func GetServiceTypes(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	Ok(w, serviceTypes)
+}
+
+func createMap(columns []string, values []interface{}) map[string]interface{} {
+	data := make(map[string]interface{})
+	for i := 0; i < len(columns); i++ {
+		fmt.Println(columns[i], values[i])
+		data[columns[i]] = values[i]
+	}
+	return data
 }
